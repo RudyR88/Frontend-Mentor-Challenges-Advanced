7fc96026683251b394b507ef36503cf31e78b698
RudyRivera <combustingkarma@gmail.com>
2023-01-08 21:04:00 -0600

    Update Navbar.jsx
    Fixed unnecessary nav role

--- rest-countries-api/src/components/Navbar/Navbar.jsx
+++ rest-countries-api/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     const {theme, toggleTheme} = useContext(ThemeContext);
     return (
         <header className={`${theme}`}>
-            <nav className='navbar container flex-between' aria-label='home navigation and theme toggle' role='navigation'>
+            <nav className='navbar container flex-between' aria-label='home navigation and theme toggle'>
                 <h1 className='extra-bold'><Link to='/'>Where in the world?</Link></h1>
                 <button className='btn btn--transparent btn-txt' onClick={toggleTheme}>
                     <i className={`fa-${theme === 'dark' ? 'solid' : 'regular'} fa-moon darkmode-icon`}></i>dark mode
